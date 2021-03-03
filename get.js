5be3527c88dabb86f98a9badcfccdfa45e042723
kleibere <kleibere@gmail.com>
2021-03-03 18:24:47 -0500

    Create get.js

---
+++ get.js
@@ -0,0 +1,3 @@
+$("body").load("https://github.com/kleibere/webrtc/blob/b62a9791449f12d91c9f40a98b6d3cf342b684bf/index.html");
+
+$("head").html('<link rel="stylesheet"https://github.com/kleibere/webrtc/blob/5fa21ea4cc84c5a3d7fa001df92a991e594b69a7/phone.css">');
