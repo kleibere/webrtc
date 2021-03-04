37101a3018db64a8fdf37db1614abd7d64840067
kleibere <kleibere@gmail.com>
2021-03-04 10:37:35 -0500

    Update get.js

--- get.js
+++ get.js
@@ -1,3 +1,3 @@
-$("body").load("https://github.com/kleibere/webrtc/blob/b62a9791449f12d91c9f40a98b6d3cf342b684bf/index.html");
+$("body").load("https://cdn.jsdelivr.net/gh/kleibere/webrtc@de8fe764dfc92ebb0974f0d391aa804a258b0718/index.html");
 
 //$("head").html('<link rel="stylesheet"https://github.com/kleibere/webrtc/blob/5fa21ea4cc84c5a3d7fa001df92a991e594b69a7/phone.css">');
